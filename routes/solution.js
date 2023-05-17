7189157822b34450e3abf87f2a4ea13436849323
Skyler Simpson <sjskyler27@gmail.com>
2023-05-17 14:34:34 -0600

    added scripts

--- routes/solution.js
+++ routes/solution.js
@@ -1,10 +1,12 @@
 const routes = require('express').Router();
 const controller = require('../controllers/solution.js');
+const cors = require('cors'); // Import the cors middleware
+
+routes.use(cors()); // Enable CORS for all routes
 
 routes.get('/', controller.getAll);
 routes.get('/:code', controller.getByCode);
 
 routes.post('/create', controller.basicCreate);
-
 
 module.exports = routes;
--- server.js
+++ server.js
@@ -1,11 +1,17 @@
 // express web server 
 const express = require('express'); // require is grabbing from node modules, it allows us to use express on this page
 const bodyParser = require('body-parser'); 
+const cors = require('cors'); // Import the cors middleware
 
 const app = express();
 
 app.use(bodyParser.json());
 app.use('/', require('./routes/solution'));
+app.use(cors()); // Enable CORS for all routes
+app.use((req, res, next) => {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    next();
+  }); // allow front end access 
 
 const port = process.env.port || 8080;
 app.listen(port)
