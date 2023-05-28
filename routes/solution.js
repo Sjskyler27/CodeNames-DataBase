41e14d616f287a7f0ea031377722285a19778978
Skyler Simpson <sjskyler27@gmail.com>
2023-05-27 23:17:19 -0600

    finished put statmeant

--- routes/solution.js
+++ routes/solution.js
@@ -4,12 +4,15 @@ const cors = require('cors'); // Import the cors middleware
 
 routes.use(cors()); // Enable CORS for all routes
 
-routes.get('/', controller.getAll);
 routes.get('/getAllCodes', controller.getAllCodes);
 routes.get('/:code', controller.getByCode);
+routes.get('/', controller.getAll);
 
 routes.post('/create', controller.basicCreate);
 routes.post('/createFromWords', controller.createFromWords);
+
+routes.put('/addToClicked/:code', controller.addToClicked);
+// routes.put('/clearClicked', controller.clearClicked);
 
 routes.delete('/deleteFirst', controller.deleteFirst);
 
