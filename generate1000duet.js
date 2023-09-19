0023921a19b077952140bb42076cb8271d4af61c
Skyler Simpson <sjskyler27@gmail.com>
2023-09-19 13:04:27 -0600

    remove auth

--- generate1000duet.js
+++ generate1000duet.js
@@ -28,6 +28,24 @@ async function generate1000() {
       clicked: ['test'],
     };
 
+    // gather the children solutions
+    const player1Solution = generateChild1(solution);
+    const player2Solution = generateChild2(solution);
+
+    solution = {
+      code: code,
+      words: shuffledWords, // now pass the shuffled
+      firstPlayer: firstPlayer,
+      Green: green,
+      Yellow: yellow,
+      Black: black,
+      player1Solution: player1Solution,
+      player2Solution: player2Solution,
+      clicked: [],
+    };
+
+    console.log(solution);
+
     solutions.push(solution);
   }
 
@@ -40,6 +58,54 @@ async function generate1000() {
       console.log('Solutions appended to JSON file.');
     }
   });
+}
+
+function generateChild1(solution) {
+  let green = [];
+  let yellow = [];
+  let black = [];
+
+  green = solution.Green.slice(0, 9); // get the first 9 greens
+
+  yellow = solution.Green.slice(9, 14); // get the last 5 greens, not the last green
+  yellow.push(...solution.Yellow.slice(0, 8)); // add 8 yellows, not the last yellows
+
+  black.push(solution.Black[0]); // get the double black
+  black.push(solution.Green[14]); // get the last green
+  black.push(solution.Yellow[8]); // get the last yellow
+
+  const player1 = {
+    Green: green,
+    Yellow: yellow,
+    Black: black,
+  };
+
+  console.log(player1);
+  return player1;
+}
+
+function generateChild2(solution) {
+  let green = [];
+  let yellow = [];
+  let black = [];
+
+  green = solution.Green.slice(6, 15); // get the last 9 greens
+
+  yellow = solution.Green.slice(1, 6); // get the first 5 greens, not the first green
+  yellow.push(...solution.Yellow.slice(1, 9)); // add 8 yellows, not the first yellows
+
+  black.push(solution.Black[0]); // get the double black
+  black.push(solution.Green[0]); // get the first green
+  black.push(solution.Yellow[0]); // get the first yellow
+
+  const player2 = {
+    Green: green,
+    Yellow: yellow,
+    Black: black,
+  };
+
+  console.log(player2);
+  return player2;
 }
 
 generate1000();
--- routes/solution.js
+++ routes/solution.js
@@ -2,14 +2,12 @@ const routes = require('express').Router();
 const controller = require('../controllers/solution.js');
 const cors = require('cors'); // Import the cors middleware
 
-const {requiresAuth} = require('express-openid-connect') // allows testing authentication
-
-
+// const {requiresAuth} = require('express-openid-connect') // allows testing authentication
 
 routes.use(cors()); // Enable CORS for all routes
 
 routes.get('/status', controller.getStatus);
-routes.get('/profile', requiresAuth(),controller.getProfile);
+routes.get('/profile', requiresAuth(), controller.getProfile);
 
 routes.get('/local/:code', controller.getLocalCode);
 routes.get('/getAllCodes', controller.getAllCodes);
@@ -23,7 +21,5 @@ routes.put('/addToClicked/:code', controller.addToClicked);
 // routes.put('/clearClicked', controller.clearClicked);
 
 routes.delete('/deleteFirst', controller.deleteFirst);
-
-
 
 module.exports = routes;
